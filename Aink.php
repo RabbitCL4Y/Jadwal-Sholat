2a34fe79972747de8c96c72375f14a46121d3000
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-27 10:36:29 +0800

    Create Aink.php

---
+++ Aink.php
@@ -0,0 +1,12 @@
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+[+]Author By   : MR.CL4Y [Muhammad Rafli]
+[+]My GitHub   : https://github.com/RabbitCL4Y
+[+]My GitLab   :https://gitlab.com/MRCL4Y
+[+]My Telegram : https://t.me/RabbitCL4Y
+[+]My Instagram: muhammadrafli_337
+[+]My Facebook : Mhmmd Rafli
+             ^_^SCRIPTKIDDIES^_^
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+  |Copyright By ©All Memb Rabbit Cyber Team |
+  |Copyright By ©All Memb KALIMANTAN SQUAD  |
+  |-----------------------------------------|
